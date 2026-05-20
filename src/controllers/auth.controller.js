80f0334519175061660dcb90c4a4d4af219de0e4
Ravi Sharma <rs5957636@gmail.com>
2026-05-20 23:28:38 +0530

    get-me

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -118,7 +118,11 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  res.clearCookie('token');
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+  });
   
   res.status(200).json({
     success: true,
